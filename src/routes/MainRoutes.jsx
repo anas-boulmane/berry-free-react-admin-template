6135c6590bccd9fb9bce9f312a4357486188c144
ayoub54321 <yammouriayoub111@gmail.com>
2023-04-03 11:08:21 +0000

    update

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -13,6 +13,7 @@ const UtilsColor = Loadable(lazy(() => import('views/utilities/Color')));
 const UtilsShadow = Loadable(lazy(() => import('views/utilities/Shadow')));
 const UtilsMaterialIcons = Loadable(lazy(() => import('views/utilities/MaterialIcons')));
 const UtilsTablerIcons = Loadable(lazy(() => import('views/utilities/TablerIcons')));
+const CSVImporter = Loadable(lazy(() => import('views/utilities/CSVImporter')));
 
 // sample page routing
 const SamplePage = Loadable(lazy(() => import('views/sample-page')));
@@ -84,8 +85,17 @@ const MainRoutes = {
     {
       path: 'sample-page',
       element: <SamplePage />
+    },
+    {
+      path: 'pouch',
+      children: [
+        {
+          path: 'pouchdb',
+          element: <CSVImporter />
+        }
+      ]
     }
-  ]
+    ]
 };
 
 export default MainRoutes;
